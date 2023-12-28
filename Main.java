1aa7e27a4aac6929a8d85cb368bc5c0ef864eb50
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2023-12-28 17:38:58 +0800

    Update Main.java
    Added try catch for exceptions

--- Main.java
+++ Main.java
@@ -68,35 +68,40 @@ public class Main {
         }
     }
     public static void main(String[] args){
-        lineGenerator();
-        System.out.println();
-        int width = 120;
-        int height = 50;
+            try{
+                lineGenerator();
+                System.out.println();
+                int width = 120;
+                int height = 50;
 
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics g = image.getGraphics();
-        g.setFont(new Font("SansSerif", Font.BOLD, 18));
+                BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+                Graphics g = image.getGraphics();
+                g.setFont(new Font("SansSerif", Font.BOLD, 18));
 
-        Graphics2D graphics = (Graphics2D) g;
-        graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-                RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-        graphics.drawString("TBD College", 3, 20);
+                Graphics2D graphics = (Graphics2D) g;
+                graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+                        RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+                graphics.drawString("TBD College", 3, 20);
 
-        for (int y = 0; y < height; y++) {
-            StringBuilder sb = new StringBuilder();
-            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    StringBuilder sb = new StringBuilder();
+                    for (int x = 0; x < width; x++) {
 
-                sb.append(image.getRGB(x, y) == -16777216 ? " " : "*");
+                        sb.append(image.getRGB(x, y) == -16777216 ? " " : "*");
 
-            }
+                    }
 
-            if (sb.toString().trim().isEmpty()) {
-                continue;
-            }
+                    if (sb.toString().trim().isEmpty()) {
+                        continue;
+                    }
 
-            System.out.println(sb);
+                    System.out.println(sb);
+                }
+                new Main();
         }
-        new Main();
+        catch(Exception e){
+            System.out.println("Something went wrong. Please try again.");
+        }
     }
     public static void lineGenerator(){
         for(int i = 0; i < 23; i++){
