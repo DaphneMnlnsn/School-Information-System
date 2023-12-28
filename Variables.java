6d8935784b6c01f0ba7f39451b980c4d73f84c9e
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2023-12-28 17:39:25 +0800

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -2,9 +2,16 @@ import java.util.*;
 
 public abstract class Variables {
     boolean loop = true;
-    String studentCourse, admitType;
+    String studentCourse, admitType, studentType, schedule, paymentTerm;
     String yearLevel, yearTerm;
     Map<String, String> studentInfo = new LinkedHashMap<>();
     static Map<Integer, Map<String, String>> enrolled = new LinkedHashMap<>();
-    static int sNum = 001;
+    static int sNum = 1000;
+    static Map<Integer, Map<String, String>> reserved = new LinkedHashMap<>();
+    public static void lineGenerator(){
+        for(int i = 0; i < 23; i++){
+            System.out.print("-----");
+        }
+        System.out.println();
+    }
 }
