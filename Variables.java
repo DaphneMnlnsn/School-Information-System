df1008314a958e893acc98dad21ac048d1d00c26
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2024-01-04 19:55:39 +0800

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -8,6 +8,8 @@ public abstract class Variables {
     static Map<Integer, Map<String, String>> enrolled = new LinkedHashMap<>();
     static int sNum = 1000;
     static Map<Integer, Map<String, String>> reserved = new LinkedHashMap<>();
+    String teacherUser[] = {"mauricio.123", "manalansan.456", "lagula.789", "cabangon.038"};
+    String teacherPass[] = {"mau0000", "man0000", "lag0000", "cab0000"};
     public static void lineGenerator(){
         for(int i = 0; i < 23; i++){
             System.out.print("-----");
