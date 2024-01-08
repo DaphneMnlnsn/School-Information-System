e388da3c49939e339d6d9e4f64029e800b176d80
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2024-01-08 18:56:16 +0800

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1861,54 +1861,60 @@ public class Admin extends Variables implements Courses  {
                 }
                 lineGenerator();
                 scn.nextLine();
-                System.out.print("Enter the name of the course (full name and abbreviation enclosed in parentheses)(0 to go back): ");
-                String courseName = scn.nextLine(); 
-                if(courseName.equals("0")){
-                    editCourse();
-                    break;
-                }
-                else{
-                    System.out.print("Enter a top skill to learn on the course: ");
-                    String courseSkill = scn.nextLine();
-                    System.out.print("Enter an opportunity when finishing the course: ");
-                    String courseOpp = scn.nextLine();
-                    int newKey = addCourse(courseName, courseSkill, courseOpp);
-                    while(true){
-                        System.out.print("Add another skill (Press Y for yes and any key for No)? ");
-                        char yn = scn.next().toUpperCase().charAt(0);
-                        if(yn == 'Y'){
-                            scn.nextLine();
-                            System.out.print("Enter a top skill to learn on the course: ");
-                            addSkill(newKey, scn.nextLine());
-                        }
-                        else{
-                            break;
-                        }
-                    }
-                    while(true){
-                        System.out.print("Add another opportunity (Press Y for yes and any key for No)? ");
-                        char yn = scn.next().toUpperCase().charAt(0);
-                        if(yn == 'Y'){
-                            scn.nextLine();
-                            System.out.print("Enter an opportunity when finishing the course: ");
-                            addOpp(newKey, scn.nextLine());
-                        }
-                        else{
-                            break;
-                        }
-                    }
-                    System.out.println("The program has been added successfully.");
-                    lineGenerator();
-                    System.out.println("Here are the currently available courses:");
-                    for(Map.Entry e: courses.entrySet()){
-                        System.out.println(e.getKey() + " - " + e.getValue());
-                    }
-                    lineGenerator();
-                    System.out.print("Press any key to go back: ");
-                    if(scn.next() != null){
+                while(true){
+                    System.out.print("Enter the name of the course (full name and abbreviation enclosed in parentheses)(0 to go back): ");
+                    String courseName = scn.nextLine(); 
+                    if(courseName.equals("0")){
                         editCourse();
+                        break;
                     }
-                }      
+                    if(!courses.containsValue(courseName)){
+                        System.out.print("Enter a top skill to learn on the course: ");
+                        String courseSkill = scn.nextLine();
+                        System.out.print("Enter an opportunity when finishing the course: ");
+                        String courseOpp = scn.nextLine();
+                        int newKey = addCourse(courseName, courseSkill, courseOpp);
+                        while(true){
+                            System.out.print("Add another skill (Press Y for yes and any key for No)? ");
+                            char yn = scn.next().toUpperCase().charAt(0);
+                            if(yn == 'Y'){
+                                scn.nextLine();
+                                System.out.print("Enter a top skill to learn on the course: ");
+                                addSkill(newKey, scn.nextLine());
+                            }
+                            else{
+                                break;
+                            }
+                        }
+                        while(true){
+                            System.out.print("Add another opportunity (Press Y for yes and any key for No)? ");
+                            char yn = scn.next().toUpperCase().charAt(0);
+                            if(yn == 'Y'){
+                                scn.nextLine();
+                                System.out.print("Enter an opportunity when finishing the course: ");
+                                addOpp(newKey, scn.nextLine());
+                            }
+                            else{
+                                break;
+                            }
+                        }
+                        System.out.println("The program has been added successfully.");
+                        lineGenerator();
+                        System.out.println("Here are the currently available courses:");
+                        for(Map.Entry e: courses.entrySet()){
+                            System.out.println(e.getKey() + " - " + e.getValue());
+                        }
+                        lineGenerator();
+                        System.out.print("Press any key to go back: ");
+                        if(scn.next() != null){
+                            editCourse();
+                        }
+                        break;
+                    }
+                    else{
+                        System.out.println("This course already exists! Please enter a non-existent course.");
+                    }
+                }  
                 break;
             }
             if(choice == '2'){
