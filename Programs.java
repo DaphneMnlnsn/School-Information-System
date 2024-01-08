55abb54ca5a810ebaae5443d0de1dfbf0bbcd7d9
Daphne <145420334+DaphneMnlnsn@users.noreply.github.com>
2024-01-08 12:16:35 +0800

    Update Programs.java

--- Programs.java
+++ Programs.java
@@ -11,19 +11,19 @@ public class Programs extends Variables implements Courses{
         while(true){
             lineGenerator();
             System.out.print("Your choice (Press 0 to go back): ");
-            int choice = scn.nextInt();
+            String choice = scn.next();
             lineGenerator();
-            if(choice == 0){
+            if(Integer.parseInt(choice) == 0){
                 new Main();
                 break;
             }
-            if(courses.containsKey(choice)){
-                System.out.println(courses.get(choice).toUpperCase());
+            if(courses.containsKey(Integer.parseInt(choice))){
+                System.out.println(courses.get(Integer.parseInt(choice)).toUpperCase());
                 System.out.println("\nTop Skills to Learn: ");
-                System.out.println(topSkills.get(courses.get(choice)));
+                System.out.println(topSkills.get(courses.get(Integer.parseInt(choice))));
             
                 System.out.println("\nCareer Opportunities: ");
-                System.out.println(opportunities.get(courses.get(choice)));
+                System.out.println(opportunities.get(courses.get(Integer.parseInt(choice))));
                 lineGenerator();
                 System.out.print("Go Back (Press Y for Yes and any key to exit system)? ");
                 char back = scn.next().toUpperCase().charAt(0);
